aadfbca018ab328e75cf139fe3d67cbd4afc86bd
tomasnanio04 <tomiinanio@gmail.com>
2023-03-01 18:29:13 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+
+
+document.getElementById('logo').addEventListener('click', function(){
+    document.getElementById('portada').style.visibility='visible';
+    document.getElementById('contenedorInfoPersonal').style.visibility = 'hidden';
+    document.getElementById('conteiner_exp').style.visibility = 'hidden';
+    document.getElementById('contenedorHab').style.visibility = 'hidden';
+    document.getElementById('formularioContacto').style.visibility = 'hidden';
+})
+
+document.getElementById('Infopersonal').addEventListener('click', function(){
+    document.getElementById('portada').style.visibility='hidden';
+    document.getElementById('contenedorInfoPersonal').style.visibility = 'visible';
+    document.getElementById('conteiner_exp').style.visibility = 'hidden';
+    document.getElementById('contenedorHab').style.visibility = 'hidden';
+    document.getElementById('formularioContacto').style.visibility = 'hidden';
+})
+
+document.getElementById('exp').addEventListener('click', function(){
+    document.getElementById('portada').style.visibility='hidden';
+    document.getElementById('contenedorInfoPersonal').style.visibility = 'hidden';
+    document.getElementById('conteiner_exp').style.visibility = 'visible';
+    document.getElementById('contenedorHab').style.visibility = 'hidden';
+    document.getElementById('formularioContacto').style.visibility = 'hidden';
+})
+
+document.getElementById('hab').addEventListener('click', function(){
+    document.getElementById('portada').style.visibility='hidden';
+    document.getElementById('contenedorInfoPersonal').style.visibility = 'hidden';
+    document.getElementById('conteiner_exp').style.visibility = 'hidden';
+    document.getElementById('contenedorHab').style.visibility = 'visible';
+    document.getElementById('formularioContacto').style.visibility = 'hidden';
+})
+
+document.getElementById('cont').addEventListener('click', function(){
+    document.getElementById('portada').style.visibility='hidden';
+    document.getElementById('contenedorInfoPersonal').style.visibility = 'hidden';
+    document.getElementById('conteiner_exp').style.visibility = 'hidden';
+    document.getElementById('contenedorHab').style.visibility = 'hidden';
+    document.getElementById('formularioContacto').style.visibility = 'visible';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
